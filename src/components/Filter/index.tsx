813ebc1579edeee2f2b8eca8d35205655577d93b
Fernando Batista <fernando.silva.externo@caixavidaeprevidencia.com.br>
2026-01-20 14:38:21 -0300

    Create home screen and components

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,26 @@
+import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native"
+import { CircleCheck } from "lucide-react-native"
+
+import { styles } from "./styles"
+import { FilterStatus } from "@/types/FilterStatus"
+
+type Props = TouchableOpacityProps & {
+    status: FilterStatus
+    isActive: boolean
+}
+
+export function Filter({ status, isActive, ...rest }: Props) {
+    return (
+        <TouchableOpacity 
+            style={[styles.container, { opacity: isActive ? 1 : 0.5 }]}
+            activeOpacity={0.8}
+            {...rest} >
+                
+                <CircleCheck size={18} color="#000000" />
+
+            <Text style={styles.title}>
+                {status === FilterStatus.DONE ? "Comprados" : "Pendentes"}
+            </Text>
+        </TouchableOpacity>
+    )
+}
